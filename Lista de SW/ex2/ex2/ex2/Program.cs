8aa6b3559b33662d846947653de3dab0b6e68714
Murilo Gama <124800631+Murilosgama@users.noreply.github.com>
2024-05-17 11:27:46 -0300

    Add files via upload

---
+++ Lista de SW/ex2/ex2/ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Compras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exemplo de uso da classe RegistroDeCompras
+            compras registro = new compras();
+            registro.AdicionarCompra(new DateTime(2024, 5, 16), "Maçã", 5.99);
+            registro.AdicionarCompra(new DateTime(2024, 5, 17), "Banana", 3.49);
+            registro.AdicionarCompra(new DateTime(2024, 5, 18), "Abacaxi", 7.99);
+
+            registro.ListarCompras();
+        }
+    }
+}
---
+++ Lista de SW/ex2/ex2/ex2/compras.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compras
+{
+    class compras
+    {
+        // Definindo uma estrutura para representar uma compra
+        struct Compra
+        {
+            public DateTime data;
+            public string produto;
+            public double valor;
+
+            public Compra(DateTime data, string produto, double valor)
+            {
+                this.data = data;
+                this.produto = produto;
+                this.valor = valor;
+            }
+        }
+
+        // Lista para armazenar as compras
+        private List<Compra> compra;
+
+        // Construtor
+        public compras()
+        {
+            compra = new List<Compra>();
+        }
+
+        // Método para adicionar uma compra
+        public void AdicionarCompra(DateTime data, string produto, double valor)
+        {
+            Compra novaCompra = new Compra(data, produto, valor);
+            compra.Add(novaCompra);
+            Console.WriteLine("Compra registrada com sucesso!");
+        }
+
+        // Método para listar todas as compras
+        public void ListarCompras()
+        {
+            Console.WriteLine("Lista de Compras:");
+
+            foreach (Compra compra in compra)
+            {
+                Console.WriteLine($"Data: {compra.data.ToShortDateString()} | Produto: {compra.produto} | Valor: R${compra.valor:F2}");
+            }
+        }
+    }
+}
