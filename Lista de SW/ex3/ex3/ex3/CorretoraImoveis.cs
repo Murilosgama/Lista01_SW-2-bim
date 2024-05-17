be90cfb8e6298be94d57f5366039a148ba3cf911
Murilo Gama <124800631+Murilosgama@users.noreply.github.com>
2024-05-17 11:35:30 -0300

    Add files via upload

---
+++ Lista de SW/ex3/ex3/ex3/CorretoraImoveis.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace corretora
+{
+    class CorretoraImoveis
+    {
+        // Definindo uma estrutura para representar um imóvel
+        struct Imovel
+        {
+            public string endereco;
+            public double preco;
+            public string tipo;
+
+            public Imovel(string endereco, double preco, string tipo)
+            {
+                this.endereco = endereco;
+                this.preco = preco;
+                this.tipo = tipo;
+            }
+        }
+
+        // Lista para armazenar os imóveis
+        private List<Imovel> imoveis;
+
+        // Construtor
+        public CorretoraImoveis()
+        {
+            imoveis = new List<Imovel>();
+        }
+
+        // Método para inserir um imóvel
+        public void InserirImovel(string endereco, double preco, string tipo)
+        {
+            Imovel novoImovel = new Imovel(endereco, preco, tipo);
+            imoveis.Add(novoImovel);
+            Console.WriteLine("Imóvel inserido com sucesso!");
+        }
+
+        // Método para alterar o preço de um imóvel
+        public void AlterarPreco(string endereco, double novoPreco)
+        {
+            Imovel imovel = imoveis.FirstOrDefault(i => i.endereco == endereco);
+            if (imovel.endereco != null)
+            {
+                imovel.preco = novoPreco;
+                Console.WriteLine($"Preço do imóvel alterado para R${novoPreco:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Imóvel não encontrado.");
+            }
+        }
+
+        // Método para listar todos os imóveis
+        public void ListarImoveis()
+        {
+            Console.WriteLine("Lista de Imóveis Disponíveis:");
+            foreach (Imovel imovel in imoveis)
+            {
+                Console.WriteLine($"Endereço: {imovel.endereco} | Preço: R${imovel.preco:F2} | Tipo: {imovel.tipo}");
+            }
+        }
+
+        // Método para calcular o valor médio dos imóveis
+        public double CalcularValorMedio()
+        {
+            if (imoveis.Count == 0)
+            {
+                return 0;
+            }
+
+            double somaPrecos = 0;
+            foreach (Imovel imovel in imoveis)
+            {
+                somaPrecos += imovel.preco;
+            }
+
+            return somaPrecos / imoveis.Count;
+        }
+    }
+}
---
+++ Lista de SW/ex3/ex3/ex3/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace corretora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CorretoraImoveis corretora = new CorretoraImoveis();
+
+            int opcao;
+            do
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Inserir Imóvel");
+                Console.WriteLine("2. Alterar Preço do Imóvel");
+                Console.WriteLine("3. Listar Imóveis");
+                Console.WriteLine("4. Calcular Valor Médio dos Imóveis");
+                Console.WriteLine("5. Sair");
+                Console.Write("Escolha uma opção: ");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write("Endereço do imóvel: ");
+                        string endereco = Console.ReadLine();
+                        Console.Write("Preço do imóvel: ");
+                        double preco = double.Parse(Console.ReadLine());
+                        Console.Write("Tipo do imóvel: ");
+                        string tipo = Console.ReadLine();
+                        corretora.InserirImovel(endereco, preco, tipo);
+                        break;
+                    case 2:
+                        Console.Write("Endereço do imóvel: ");
+                        string enderecoAlterarPreco = Console.ReadLine();
+                        Console.Write("Novo preço do imóvel: ");
+                        double novoPreco = double.Parse(Console.ReadLine());
+                        corretora.AlterarPreco(enderecoAlterarPreco, novoPreco);
+                        break;
+                    case 3:
+                        corretora.ListarImoveis();
+                        break;
+                    case 4:
+                        double valorMedio = corretora.CalcularValorMedio();
+                        Console.WriteLine($"O valor médio dos imóveis é R${valorMedio:F2}");
+                        break;
+                    case 5:
+                        Console.WriteLine("Saindo do programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+
+            } while (opcao != 5);
+        }
+    }
+}
