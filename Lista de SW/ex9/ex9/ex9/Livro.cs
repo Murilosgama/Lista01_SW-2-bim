a50a1c7ffd799944b8be7aee92a9d2d641fcf2de
Murilo Gama <124800631+Murilosgama@users.noreply.github.com>
2024-05-17 11:48:42 -0300

    Add files via upload

---
+++ Lista de SW/ex9/ex9/ex9/Livro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace editora
+{
+    class Livro
+    {
+        private string titulo;
+        private string autor;
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    titulo = value;
+                }
+                else
+                {
+                    throw new ArgumentException("O título do livro não pode estar vazio.");
+                }
+            }
+        }
+
+        public string Autor
+        {
+            get { return autor; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    autor = value;
+                }
+                else
+                {
+                    throw new ArgumentException("O autor do livro não pode estar vazio.");
+                }
+            }
+        }
+
+        public Livro(string titulo, string autor)
+        {
+            Titulo = titulo;
+            Autor = autor;
+        }
+    }
+
+}
---
+++ Lista de SW/ex9/ex9/ex9/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace editora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Livro livro = new Livro("O Senhor dos Anéis", "J.R.R. Tolkien");
+                Console.WriteLine($"Título do livro: {livro.Titulo}");
+                Console.WriteLine($"Autor: {livro.Autor}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Erro ao criar livro: {e.Message}");
+            }
+        }
+    }
+}
